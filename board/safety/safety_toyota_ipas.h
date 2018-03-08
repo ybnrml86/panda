8ed3e9504c0787de954670913d33dc5e3c620ec3
Jennifer Strange <strange.jennifer@yandex.com>
2018-03-08 14:53:05 -0800

    dat forwarding ish

--- board/safety/safety_toyota_ipas.h
+++ board/safety/safety_toyota_ipas.h
@@ -1,18 +1,18 @@
 
 static int toyota_ipas_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
-  if (bus_num == 1) {
-    return 3;
+  if (bus_num == 0) {
+    return 2;
   }
-  if (bus_num == 3) {
-    return 1;
+  if (bus_num == 2) {
+    return 0;
   }
   return -1;
 }
 
 const safety_hooks toyota_ipas_hooks = {
-  .init = nooutput_init,
+  .init = alloutput_init,
   .rx = default_rx_hook,
-  .tx = nooutput_tx_hook,
+  .tx = alloutput_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
   .fwd = toyota_ipas_fwd_hook,
 };
