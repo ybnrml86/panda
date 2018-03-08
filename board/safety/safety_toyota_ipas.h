131de08d8971c325eabb383047b672936b1fb189
Jennifer Strange <strange.jennifer@yandex.com>
2018-03-08 15:01:14 -0800

    mask out the speed

--- board/safety/safety_toyota_ipas.h
+++ board/safety/safety_toyota_ipas.h
@@ -1,6 +1,12 @@
 
 static int toyota_ipas_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   if (bus_num == 0) {
+    if ((to_fwd->RIR>>21) == 0xb4) {
+      // only mask out the speed?
+      to_fwd->RDLR = 0;
+      to_fwd->RDHR = 0xbc000000;
+    }
+
     return 2;
   }
   if (bus_num == 2) {
