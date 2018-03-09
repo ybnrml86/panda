522cc0a7b34635b3722e12d1b8ae83e606819161
Riccardo <riccardo.biasini@gmail.com>
2018-03-09 14:08:57 -0800

    minor aest changes

--- board/safety/safety_toyota_ipas.h
+++ board/safety/safety_toyota_ipas.h
@@ -3,13 +3,14 @@ static int toyota_ipas_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   if (bus_num == 0) {
     if ((to_fwd->RIR>>21) == 0xb4) {
       // cap the speed at 6kph (600)
-      int speed = ((to_fwd->RDHR) & 0xFF00) | ((to_fwd->RDHR >> 16) & 0xFF);
-      if (speed > 600) {
-        speed = 600;
+      const int speed_max = 600;
+      int speed = ((to_fwd->RDHR) & 0xff00) | ((to_fwd->RDHR >> 16) & 0xff);
+      if (speed > speed_max) {
+        speed = speed_max;
       }
       int checksum = (0xb4 + 8 + speed) & 0xff;
       to_fwd->RDLR = 0;
-      to_fwd->RDHR = (checksum << 24) + ((speed & 0xFF) << 16) + (speed & 0xFF00);
+      to_fwd->RDHR = (checksum << 24) + ((speed & 0xff) << 16) + (speed & 0xff00);
     }
 
     return 2;
