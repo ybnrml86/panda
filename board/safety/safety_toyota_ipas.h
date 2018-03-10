2e6937f7183eb1654b7c0e6dda35d507ca2aa5d6
Riccardo <riccardo.biasini@gmail.com>
2018-03-09 16:02:19 -0800

    Toyota ipas: fixed checksum and increased max speed

--- board/safety/safety_toyota_ipas.h
+++ board/safety/safety_toyota_ipas.h
@@ -2,13 +2,13 @@
 static int toyota_ipas_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   if (bus_num == 0) {
     if ((to_fwd->RIR>>21) == 0xb4) {
-      // cap the speed at 6kph (600)
-      const int speed_max = 600;
+      // cap the speed at 10.25kph (1025). 1050 has been tested to be too high
+      const int speed_max = 1025;
       int speed = ((to_fwd->RDHR) & 0xff00) | ((to_fwd->RDHR >> 16) & 0xff);
       if (speed > speed_max) {
         speed = speed_max;
       }
-      int checksum = (0xb4 + 8 + speed) & 0xff;
+      int checksum = (0xb4 + 8 + speed + (speed >> 8)) & 0xff;
       to_fwd->RDLR = 0;
       to_fwd->RDHR = (checksum << 24) + ((speed & 0xff) << 16) + (speed & 0xff00);
     }
