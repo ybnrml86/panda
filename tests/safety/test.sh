188adc47f1e4b5bccd92a0bdc5b0d199dc5774f5
Martin Lillepuu <martin@mlp.ee>
2020-05-21 09:25:24 +0300

    enable verbose unit tests

--- tests/safety/test.sh
+++ tests/safety/test.sh
@@ -14,5 +14,5 @@ set -e
 for hw_type in 0 1 2 3 4 5
 do
   echo "Testing HW_TYPE: $hw_type"
-  HW_TYPE=$hw_type python -m unittest discover .
+  HW_TYPE=$hw_type python -m unittest -v discover .
 done
