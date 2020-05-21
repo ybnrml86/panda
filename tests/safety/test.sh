212d945a6af56e75e76194104c8d878702bd367e
Martin Lillepuu <martin@mlp.ee>
2020-05-21 11:11:26 +0300

    more verbose unittest test

--- tests/safety/test.sh
+++ tests/safety/test.sh
@@ -14,6 +14,5 @@ set -e
 for hw_type in 0 1 2 3 4 5
 do
   echo "Testing HW_TYPE: $hw_type"
-  HW_TYPE=$hw_type python -m unittest discover .
-  HW_TYPE=$hw_type python -m unittest -v tests/safety/test_subaru_legacy.py
+  HW_TYPE=$hw_type python -m unittest discover -v
 done
