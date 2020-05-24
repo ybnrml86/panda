7ddd2f43399687d62f17e0242096a6abad913d89
Martin Lillepuu <martin@mlp.ee>
2020-05-24 10:59:01 +0300

    update tests order in /run_docker_tests_local.sh

--- run_docker_tests_local.sh
+++ run_docker_tests_local.sh
@@ -6,6 +6,6 @@ RUN="docker run --shm-size 1G --rm panda /bin/sh -c"
 #docker pull docker.io/commaai/panda:latest
 docker build --cache-from docker.io/commaai/panda:latest -t panda -f Dockerfile.panda .
 
-$RUN "cd /tmp/openpilot/panda/tests/safety && ./test.sh"
 $RUN "cd /tmp/openpilot/panda/tests/linter_python/ && ./flake8_panda.sh"
 $RUN "cd /tmp/openpilot/panda/tests/linter_python/ && ./pylint_panda.sh"
+$RUN "cd /tmp/openpilot/panda/tests/safety && ./test.sh"
